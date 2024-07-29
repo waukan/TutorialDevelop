3f75a3f5562b6d78a492168401190d20ceef5e4a
waukan <uni@waukan.sakura.ne.jp>
2024-07-30 01:19:38 +0900

    5

--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -1,5 +1,8 @@
 package com.techacademy.entity;
 
+import org.hibernate.validator.constraints.Length;
+import org.springframework.transaction.annotation.Transactional;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,7 +10,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PreRemove;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -27,18 +37,38 @@ public class User {
 
     /** 名前。20桁。null不許可 */
     @Column(length = 20, nullable = false)
+    @NotEmpty
+    @Length(max=20)
     private String name;
 
     /** 性別。2桁。列挙型(文字列) */
     @Column(length = 2)
     @Enumerated(EnumType.STRING)
+    @NotNull
     private Gender gender;
 
     /** 年齢 */
+    @Min(0)
+    @Max(120)
     private Integer age;
 
     /** メールアドレス。50桁。null許可 */
     @Column(length = 50)
+    @Email
+    @Length(max=50)
     private String email;
+
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
+
+    /** レコードが削除される前に行う処理*/
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        // 認証エンティティからuserを切り離す
+        if(authentication!=null) {
+            authentication.setUser(null);
+        }
+    }
 
 }
